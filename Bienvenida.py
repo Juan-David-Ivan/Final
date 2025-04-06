de60ddb8ae0ac1cfc212cf9f2513f610bb911f3a
Juan David Ivan <damx@damx>
2025-04-06 23:26:50 +0200

    actualizacion del codigo DE BIENVENIDA

--- Bienvenida.py
+++ Bienvenida.py
@@ -3,11 +3,18 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "INFORMACION"
 
+    def volver(e):
+        page.go("/formulario")
+        page.update()
+
+    volver_btn = ft.ElevatedButton(text="Adelante", on_click=volver)
+
     columna = ft.Column(
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         alignment=ft.CrossAxisAlignment.CENTER,
         controls=[
-            ft.Text("BIENVENIDO A MI PÁGINA WEB RAFA", color="white"),
+            ft.Text("BIENVENIDO A MI PÁGINA WEB, RAFA!!🍺🍺🍺🍺", color="white"),
+            volver_btn
 
         ]
     )
